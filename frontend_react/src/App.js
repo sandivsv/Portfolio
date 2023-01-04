a509d2e7770602d456813600f7336c4a83105e35
Sandivsv <sandeepkmr7379@gmail.com>
2023-01-05 01:03:44 +0530

    Make containers

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { About, Navbar, Footer, Header, Skills, Testimonial, Work } from './container';
+// import { Navbar } from './components';
 import './App.scss';
 
 const App = () => (
   <div className="app">
     My App
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
   </div>
 );
 
---
+++ frontend_react/src/container/index.js
@@ -0,0 +1,17 @@
+import About from './About';
+import Footer from './Footer';
+import Header from './Header';
+import Skills from './Skills';
+import Testimonial from './Testimonial';
+import Work from './Work';
+import Navbar from './Navbar'
+
+export {
+  About,
+  Navbar,
+  Footer,
+  Header,
+  Skills,
+  Testimonial,
+  Work,
+};
